12417cf58b854f9e2150fc7700bab39b81fab39d
Michael Dayah <michael@dayah.com>
2018-08-19 16:48:35 -0400

    New layout. Ready for migration.

--- dayah.js
+++
@@ -1,9 +0,0 @@
-function swap(id) {
-	if (document.getElementById(id).style.display == "block") {
-		document.getElementById(id).style.display = "none";
-		document.getElementById(id + "-Text").style.display = "none";
-	} else {
-		document.getElementById(id).style.display = "block";
-		document.getElementById(id + "-Text").style.display = "inline";
-	}
-}
